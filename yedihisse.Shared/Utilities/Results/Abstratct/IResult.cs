995cb160125a88b942ea6e4cf7edd84e4ad39499
mresitgokce1 <gQaZ77o16o9b>
2021-11-13 12:54:25 +0300

    Created result structure

---
+++ yedihisse.Shared/Utilities/Results/Abstratct/IResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using yedihisse.Shared.Utilities.Results.Complex_Type;
+
+namespace yedihisse.Shared.Utilities.Results.Abstratct
+{
+    public interface IResult
+    {
+        public ResultStatus ResultStatus { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+    }
+}
---
+++ yedihisse.Shared/Utilities/Results/Concrete/DataResult.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using yedihisse.Shared.Utilities.Results.Abstratct;
+using yedihisse.Shared.Utilities.Results.Complex_Type;
+
+namespace yedihisse.Shared.Utilities.Results.Concrete
+{
+    public class DataResult<T> : IDataResult<T>
+    {
+        public DataResult(ResultStatus resultStatus, T data)
+        {
+            ResultStatus = resultStatus;
+            Data = data;
+        }
+
+        public DataResult(ResultStatus resultStatus, string message, T data)
+        {
+            ResultStatus = resultStatus;
+            Data = data;
+            Message = message;
+        }
+        public DataResult(ResultStatus resultStatus, string message, T data, Exception exception)
+        {
+            ResultStatus = resultStatus;
+            Data = data;
+            Message = message;
+            Exception = exception;
+        }
+        public ResultStatus ResultStatus { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+        public T Data { get; }
+    }
+}
